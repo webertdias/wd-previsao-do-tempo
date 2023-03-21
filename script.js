ed7c529bdc158ef403ba599eaa91618c97522c9c
Webert Dias <wdwebert@gmail.com>
2023-03-20 23:13:18 -0300

    Trabalhando no js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var cidade = document.getElementById("lugar");
+const place = document.querySelector("place");
+const button = document.querySelector("button");
+const city = document.querySelector("city");
+const degree = document.querySelector("degree");
+
+addEventListener(click, function buscarCidade(){
+    let url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'(
+    place.value
+    )})
