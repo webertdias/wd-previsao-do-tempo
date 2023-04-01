4467871920fb531298707410ebff5a724b13f29e
Webert Dias <wdwebert@gmail.com>
2023-04-01 19:33:31 -0300

    Acrescentando caracteristicas do clima

--- script.js
+++ script.js
@@ -4,33 +4,10 @@ const img = document.querySelector("img");
 
 const city = document.querySelector("#city");
 const degree = document.querySelector("#degree");
+const country = document.querySelector("#country");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
 
-const content = querySelector(".content");
 
-button.addEventListener("click", () =>{
-    if (input.value) return;
-    getWeatherData();
-});
 
-async function getWeatherData(){
-    let urlApi = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key';
-
-    try{
-        await fetch(urlApi)
-        .then((res) => res.json())
-        .then((data)=>{
-            if(data?.cod && data.cod === "404"){
-                return alert("Cidade não encontrada");
-            }
-            loadWhatherInfo(data);
-        })
-    }catch(error){
-        alert(error);
-    }
-}
-function loadWhatherInfo(data){
-    city.innerHTML = '${data.name}, $(data.sys.country)';
-    degree.innerHTML = 'Temperatura: ${Math.floor(data.main.temp)}ºC';
-    img.src = 'http://openweathermap.org/img/wn/${data.weather[0].icon}@2.png';
-    content.style.display = "flex";
-}
+//ddf85b4f6c1ce572966991a36cc517a8
