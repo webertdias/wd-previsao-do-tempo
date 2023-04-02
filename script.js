bdee5e6a6d6aceba1ad84c06699e6e8cef2b7696
Webert Dias <wdwebert@gmail.com>
2023-04-02 16:34:12 -0300

    Trabalhando funções e eventos

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-const img = document.querySelector("img");
+// variaveis e seleção de elementos DOM
+const apiKey = "ddf85b4f6c1ce572966991a36cc517a8";
 
-const city = document.querySelector("#city");
+const cityInput = document.querySelector("#place");
+const search = document.querySelector("#search");
+
+
 const degree = document.querySelector("#degree");
 const country = document.querySelector("#country");
 const humidity = document.querySelector("#humidity");
 const wind = document.querySelector("#wind");
 
 
+//funções
 
-//ddf85b4f6c1ce572966991a36cc517a8
+
+const showWeatherData = (city) => {
+
+    console.log(city);
+}
+
+
+//eventos
+search.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+})
